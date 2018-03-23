4d9a04c66383803f985ab0ef945aa7b678098ae0
Olanrewaju Ebenezer <olanrewaju.ebenezer@VentureGardenGroup.Local>
2018-03-23 16:50:03 +0100

    Added state stuffs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {name: 'Ayo', names: []}
+  }
+
+  change(event) {
+    event.persist();
+    this.setState({name: event.target.value});
+  }
+
+  click(event) {
+    if (this.state.name === '') {
+      return;
+    }
+
+    event.persist();
+    this.setState({names: [...this.state.names, this.state.name]}, ()=>this.setState({name: ''}));
+  }
+
   render() {
     return (
       <div className="wrapper">
@@ -11,16 +31,15 @@ class App extends Component {
         </header>
         <div className="flex-wrapper">
         <ul className="list">
-          <li>foo</li>
-          <li>foo</li>
-          <li>foo</li>
-          <li>foo</li>
+          {
+            this.state.names.map(item => <li>{item}</li>)
+          }
         </ul>
         
         <div className="">
         <div className="center">
-        <input type="text"  className="mr-10" />
-          <button className="button" >foo</button>
+        <input type="text"  className="mr-10" value={this.state.name} onChange={this.change.bind(this)}/>
+          <button className="button" onClick={this.click.bind(this)}>foo</button>
         </div>
           
         </div>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App  names = {['Seun', 'Tobi', 'Femi']}/>, document.getElementById('root'));
 registerServiceWorker();
